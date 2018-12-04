f266eb378035f85d0501fa0d16d31cab802791ba
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2018-12-04 13:37:21 -0800

    added 404 page

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,21 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize';
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Chat from './chat';
+import Home from './home';
 import Nav from './nav/nav';
+import NotFound from './404';
 
 const App = () => (
     <div>
         <Nav />
         <div className="container">
-
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/chat" component={Chat} />
+                <Route component={NotFound} />
+            </Switch> 
         </div>
     </div>
 );
