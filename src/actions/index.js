1d0791295d30ba474e240c3f3fda6b578d89a908
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2018-12-07 11:46:17 -0800

    implemented firebase files and folders; populated initial firebase massages

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,18 @@
 import types from './types';
+import { db } from '../firebase';
+
+// export const getAllMessages = () => async dispatch =>
+// --OR --
+
+export function getAllMessages(){
+    return async function(dispatch){
+        db.ref('/messages').on('value', ( snapshot ) => {
+            
+            dispatch({
+                type: types.GET_CHAT_MESSAGES,
+                messages: snapshot.val()
+            });
+        });
+    }
+}
+
